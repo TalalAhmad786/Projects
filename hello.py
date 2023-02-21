327e4a0f9ae5f7f190eaeeff962d240b345f14cc
TalalAhmad786 <99980289+TalalAhmad786@users.noreply.github.com>
2023-02-21 21:23:37 +0500

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,9 @@
+""" x = ["a", "b", "c", "d", "e", "f"]
+x[1] = "r"
+print(x)
+x[2:] = ["s", "t"]
+print(x) """
+areas = ["hallway", 11.25, "kitchen", 18.0, "chill zone", 20.0,
+         "bedroom", 10.75, "bathroom", 10.50]
+areas= areas+ ["poolhouse", 24.5]
+print(areas)
